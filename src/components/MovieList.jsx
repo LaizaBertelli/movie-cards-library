e1062862d7c8feaab7fb3c2b03de7037446e9b8c
LaizaBertelli <laizabertelli@hotmail.com>
2021-10-06 11:29:07 -0300

    Adiciona validação de tipos para as props de MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,7 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import PropType from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
@@ -15,5 +15,14 @@ class MovieList extends React.Component {
     );
   }
 }
+
+// Se nenhum valor for passado para a props o default usado na validação será um array de objetos
+MovieList.defaultProps = {
+  movies: [{}],
+};
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default MovieList;
