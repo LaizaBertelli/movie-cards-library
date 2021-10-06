a9de60e1b0abb5819b1fff76b2a7b659f322c0d9
LaizaBertelli <laizabertelli@hotmail.com>
2021-10-06 10:52:34 -0300

    Cria e renderiza o componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    console.log(this.props);
+    return <p>{movie.title}</p>;
+  }
+}
+
+export default MovieCard;
