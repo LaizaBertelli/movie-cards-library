600bafd654c24a2103077638835510e7db0a0cb0
LaizaBertelli <laizabertelli@hotmail.com>
2021-10-06 11:30:56 -0300

    Adiciona renderização para elementos passados ao MovieCard e tipagem das props

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,34 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    console.log(this.props);
-    return <p>{movie.title}</p>;
+    return (
+      <section>
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+        <img src={ movie.imagePath } alt={ movie.title } />
+
+      </section>
+    );
   }
 }
+
+// Se nenhum valor for passado para a props o default usado na validação será um objeto vazio
+MovieCard.defaultProps = {
+  movie: {},
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }),
+};
 
 export default MovieCard;
