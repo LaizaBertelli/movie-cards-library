9ac8599b708f814d0df8efc69c349a7ea85f05b0
LaizaBertelli <laizabertelli@hotmail.com>
2021-10-06 09:59:41 -0300

    Cria e renderiza o componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return <p>Teste</p>;
+  }
+}
+
+export default MovieList;
